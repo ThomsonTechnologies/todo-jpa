827533cf043594ec986cb4d0f7f038a20e064746
thomson-mathew <thomsonscode@gmail.com>
2019-02-16 13:23:31 -0600

    Added project

---
+++ src/main/java/com/thomsoncodes/todo/TodoJpaApplication.java
@@ -0,0 +1,14 @@
+package com.thomsoncodes.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJpaApplication.class, args);
+	}
+
+}
+
